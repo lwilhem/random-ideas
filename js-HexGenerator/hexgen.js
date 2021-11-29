a8d001134a516e0bfbc9ca1c17d7a55322fa2144
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2021-11-29 16:59:33 +0100

    :memo: added notes

--- js-HexGenerator/hexgen.js
+++ js-HexGenerator/hexgen.js
@@ -1,17 +1,15 @@
-//var formula = Math.floor(Math.random()*16777215).toString(16);
-
 const trigger = document.getElementById('trigger');
 const hexColor = document.getElementById('color-output');
 const hexCode = document.getElementById('hex-code');
 
-const hexData = [];
+const hexData = []; // will be useful later to store & display multiple hex
 
-function hexWriter(randomizerResult){
+function hexWriter(randomizerResult){ //write hexGenerator result with a valdi format 
     hexColor.style.backgroundColor = ["#" + randomizerResult];
     hexCode.innerHTML = ["#" + randomizerResult];
 }
 
 trigger.onclick = function hexGenerator(result){
-    result = Math.floor(Math.random()*16777215).toString(16);
+    result = Math.floor(Math.random()*16777215).toString(16); //math functions to generate a valid hex code
     hexWriter(result);
 }
