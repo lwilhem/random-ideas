33b0477327b3c515aebb2c0a1af60f606725ba50
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2021-11-29 16:57:03 +0100

    :hammer: added trigger detection & added results writing in HTML file

--- js-HexGenerator/hexgen.js
+++ js-HexGenerator/hexgen.js
@@ -1,7 +1,17 @@
-const trigger = document.getElementById('trigger');
-let outputColor = document.getElementById('color-ouput');
-let outputCode = document.getElementById('hex-code');
+//var formula = Math.floor(Math.random()*16777215).toString(16);
 
-trigger.addEventListener("click", function hexGenerator(){
-    var formula = Math.floor(Math.random()*16777215).toString(16);
-});
+const trigger = document.getElementById('trigger');
+const hexColor = document.getElementById('color-output');
+const hexCode = document.getElementById('hex-code');
+
+const hexData = [];
+
+function hexWriter(randomizerResult){
+    hexColor.style.backgroundColor = ["#" + randomizerResult];
+    hexCode.innerHTML = ["#" + randomizerResult];
+}
+
+trigger.onclick = function hexGenerator(result){
+    result = Math.floor(Math.random()*16777215).toString(16);
+    hexWriter(result);
+}
