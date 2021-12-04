d45c3216b199e85c63fe137dcbf78997a36f0bec
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2021-12-04 16:18:42 +0100

    :test_tube: added error check

--- js-HexGenerator/hexgen.js
+++ js-HexGenerator/hexgen.js
@@ -1,12 +1,21 @@
-const trigger = document.getElementById('trigger');
 const hexColor = document.getElementById('color-output');
 const hexCode = document.getElementById('hex-code');
+const trigger = document.getElementById('trigger')
 
 const hexData = []; // will be useful later to store & display multiple hex
 
-function hexWriter(randomizerResult){ //write hexGenerator result with a valdi format 
+function hexWriter(randomizerResult){ //write hexGenerator result with a valid format
+    
+    let getStyle = getComputedStyle(hexColor);
+    let comparatorTarget = getStyle.backgroundColor;
+
+    if(randomizerResult == comparatorTarget){
+        return;
+    }
+    else{
     hexColor.style.backgroundColor = ["#" + randomizerResult];
     hexCode.innerHTML = ["#" + randomizerResult];
+    }
 }
 
 trigger.onclick = function hexGenerator(result){
@@ -14,8 +23,11 @@ trigger.onclick = function hexGenerator(result){
     hexWriter(result);
 }
 
+
 //Future plans : How I will improve this:
 //  - Keyboard detection with/over button pressing,
 //  - Add an error check if the same color is generated twice in a row(or is already generated, see below),
 //  - possibility to create a color palette, with JSON functionality,
 //  - Improve code readability.
+
+
