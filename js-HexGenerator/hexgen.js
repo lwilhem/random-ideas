4120fd1934467f13c6f4d801722340ad6a3fa845
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2021-12-04 18:30:17 +0100

    :lipstick: :sparkles: :memo: Added keyboard Detection

--- js-HexGenerator/hexgen.js
+++ js-HexGenerator/hexgen.js
@@ -1,6 +1,5 @@
 const hexColor = document.getElementById('color-output');
 const hexCode = document.getElementById('hex-code');
-const trigger = document.getElementById('trigger')
 
 const hexData = []; // will be useful later to store & display multiple hex
 
@@ -10,6 +9,7 @@ function hexWriter(randomizerResult){ //write hexGenerator result with a valid f
     let comparatorTarget = getStyle.backgroundColor;
 
     if(randomizerResult == comparatorTarget){ //Check if the Generated value is the same that the one applied
+        alert('Oups! that was the same color!');
         return;
     }
     else{
@@ -18,16 +18,19 @@ function hexWriter(randomizerResult){ //write hexGenerator result with a valid f
     }
 }
 
-trigger.onclick = function hexGenerator(result){
-    result = Math.floor(Math.random()*16777215).toString(16); //math functions to generate a valid hex code
-    hexWriter(result);
-}
+document.addEventListener('keydown', event => {
+    if(event.isComposing || event.key === ' '){
+        result = Math.floor(Math.random()*16777215).toString(16); //math functions to generate a valid hex code
+        hexWriter(result);
+    }
+    else{
+        return;
+    }
+})
 
 
 //Future plans : How I will improve this:
-//  - Keyboard detection with/over button pressing,
+//  - Keyboard detection with/over button pressing, DONE => Space key
 //  - Add an error check if the same color is generated twice in a row(or is already generated, see below), DONE
 //  - possibility to create a color palette, with JSON functionality,
 //  - Improve code readability.
-
-
